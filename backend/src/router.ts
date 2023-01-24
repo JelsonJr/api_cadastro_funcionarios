5ac23fecd56b014102eb85a9ce6612dd67376443
JelsonJr <jelson.rodrigues.junior@gmail.com>
2023-01-24 20:22:49 -0300

    initial commit

---
+++ backend/src/router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { firstController } from "./app/controller/FirstController";
+
+const router: Router = Router()
+router.get("/", firstController.home);
+
+export { router };
