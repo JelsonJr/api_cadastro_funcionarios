887d72e3d49311d72a4eaadd6da8b17da3528a31
JelsonJr <jelson.rodrigues.junior@gmail.com>
2023-02-03 11:19:32 -0300

    mais ajustes nas validacoes e criacao da rota delete

--- backend/src/app/controllers/FuncionarioController.ts
+++ backend/src/app/controllers/FuncionarioController.ts
@@ -2,12 +2,18 @@ import { Request, Response } from "express";
 import { Funcionario } from "../models/Funcionario";
 import { verificaCPF } from "../utils/CPFValidator";
 import { validaErro } from "../utils/ErrorValidator";
-import { Document, Types } from 'mongoose';
 import IFuncionario from '../models/interfaces/IFuncionario';
 
 class FuncionarioController {
 
     public async cadastrar(req: Request, res: Response) {
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                erro: "Requisição sem dados",
+                success: false
+            });
+        }
+
         const cpf_verificado = verificaCPF(req.body.cpf);
         if (cpf_verificado.invalido) return res.status(400).json({ erro: cpf_verificado.erro, success: false });
 
@@ -44,9 +50,10 @@ class FuncionarioController {
     }
 
     public async listagemFuncionarios(req: Request, res: Response) {
-        //fazer paginacao
-        //validar erros
-        const funcionarios = await Funcionario.find({ situacao: true }).limit(req.body.limit || 10).skip(req.body.skip || 0);
+        const funcionarios = await Funcionario.find({ situacao: true })
+            .limit(req.body.limit || 10)
+            .skip(req.body.skip || 0);
+            
         if (!funcionarios || funcionarios === null)
             return res.status(500).json({
                 erro: 'Não foi possível fazer a busca dos funcionários',
@@ -57,25 +64,28 @@ class FuncionarioController {
     }
 
     public async listaFuncionario(req: Request, res: Response) {
-        const cpf_verificado = verificaCPF(req.body.cpf);
-        if (cpf_verificado.invalido)
-            return res.status(400).json({
-                erro: cpf_verificado.erro,
-                success: false,
-            });
-
         let funcionario: IFuncionario | null;
-        try {
-            funcionario = req.params.id
-                ? await Funcionario.findOne({ _id: req.params.id })
-                : await Funcionario.findOne({ cpf: req.body.cpf.replace(/\.|-/g, "") });
 
-            if (funcionario === null)
+        try {
+            if (req.params.id) {
+                funcionario = await Funcionario.findOne({ _id: req.params.id });
+            } else {
+                const cpf_verificado = verificaCPF(req.body.cpf);
+                if (cpf_verificado.invalido)
+                    return res.status(400).json({
+                        erro: cpf_verificado.erro,
+                        success: false,
+                    });
+                
+                funcionario = await Funcionario.findOne({ cpf: req.body.cpf.replace(/\.|-/g, "") });
+            }
+
+            if (funcionario === null) 
                 return res.status(404).json({
-                    erro: 'Funcionário não encontrado',
+                    erro: "Funcionário não encontrado",
                     success: false,
                 });
-
+            
             return res.status(200).json({ funcionario, success: true });
         } catch (erro: any) {
             const resposta = validaErro(erro);
@@ -84,6 +94,10 @@ class FuncionarioController {
                 success: false,
             });
         }
+    }
+
+    public async desativaFuncionario(req: Request, res: Response) {
+        console.log(req.body.cpf)
     }
 }
 
--- backend/src/app/routes/funcionariosRouter.ts
+++ backend/src/app/routes/funcionariosRouter.ts
@@ -3,9 +3,10 @@ import { Router } from "express";
 
 const funcionariosRoutes: Router = Router();
 
+funcionariosRoutes.get('/funcionarios', funcionarioController.listagemFuncionarios)
 funcionariosRoutes.get('/funcionario/:id', funcionarioController.listaFuncionario)
 funcionariosRoutes.get('/funcionario', funcionarioController.listaFuncionario)
-funcionariosRoutes.get('/funcionarios', funcionarioController.listagemFuncionarios)
+funcionariosRoutes.delete('/funcionario/:id', funcionarioController.desativaFuncionario)
 funcionariosRoutes.post('/cadastro', funcionarioController.cadastrar)
 
 export default funcionariosRoutes;
